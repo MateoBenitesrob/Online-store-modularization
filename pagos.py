6663a9297f1c861f551bfccb66a1ece033c2bd53
maTeoBenites <mateorob.teo@gmail.com>
2026-08-03 21:46:45 -0500

    Enhance payment validation and processing functions
    Added support for Visa and Mastercard payment methods, implemented payment processing and confirmation functions.

--- pagos.py
+++ pagos.py
@@ -2,11 +2,39 @@
 #⏳ Procesar pago
 #⏳ Confirmar pago
 
-def validar_metodo_pago(metodo_pago):
 
-    metodos_validos = ["Tarjeta", "Yape", "Plin", "PayPal"]
+# Se evalúa el método de pago
+def validar_metodo_pago(metodo_pago):
+    metodos_validos = ["Tarjeta",
+                        "Yape",
+                          "Plin",
+                            "PayPal",
+                              "Visa",
+                                "Mastercard"
+                                    ]
     if metodo_pago in metodos_validos:
         return True
     return False
 validar_metodo_depago = validar_metodo_pago("Visa")
 print(validar_metodo_depago)
+
+
+# Se procesa el pago
+def procesar_pago(metodo_pago, monto):
+
+    if validar_metodo_pago(metodo_pago):
+      print(f"Procesando pago de S/{monto} con {metodo_pago}...")
+      return True
+    else:
+      print("el sistema no pudo aceptar su método de pago")
+      return False
+procesar_unpago = procesar_pago("Visa", 11111)
+
+
+# Finalmente el pago se confirma
+def confirmar_pago(pago_exitoso):
+   if pago_exitoso:
+      print("Se a realizado exitosamente el pago")
+      return True
+   else:
+      print("Ha ocurrido un error al confirmar el pago, inténtelo de nuevo")
