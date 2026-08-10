6c7b75db742d4ecfc16c3f80b2bf620dc07a386c
maTeoBenites <mateorob.teo@gmail.com>
2026-08-09 19:35:51 -0500

    Refactor realizar_compra for better payment validation
    Refactor realizar_compra function to improve payment processing and error handling.

--- compra.py
+++ compra.py
@@ -3,31 +3,32 @@ from pagos import validar_metodo_pago # Necesita método de pago
 from precio import subtotal # Necesita carrito
 from precio import total # Necesita carrito y subtotal
 from pagos import procesar_pago # Necesita método de pago y TOTAL
-from pagos import confirmar_pago # Necesita método de pago
+from pagos import confirmar_pago # confirmar pago exitoso
 from factura import generar_factura # Necesita carrito, total y método de pago
 
 def realizar_compra(carrito, metodo_pago):
-    if not carrito:
-        print("Error: El carrito está vacío.")
-        return False
-
     if validar_metodo_pago(metodo_pago):
-        
+
         monto_subtotal = subtotal(carrito)
-        monto_total = total(carrito, monto_subtotal) 
-        
-        
-        procesar_pago(metodo_pago, monto_total)
-        confirmar_pago(metodo_pago)
-        
-        
-        generar_factura(carrito, monto_total, metodo_pago)
-        
-        print("La compra fue realizada con éxito. Gracias por comprar.")
-        return True # Indicamos que la operación fue un éxito
+        monto_total = total(carrito, monto_subtotal)
+
+        pago_exitoso = procesar_pago(metodo_pago, monto_total)
+
+        if not pago_exitoso:
+            return False
+
+        pago_confirmado = confirmar_pago(pago_exitoso)
+
+        if not pago_confirmado:
+            return False
+
+        factura_generada = generar_factura(
+            carrito, monto_total, metodo_pago
+        )
+
+        return factura_generada
     else:
-        print("Error: Método de pago no válido.")
-        return False
+        ("ERROR: método de pago no registado en el sistema, inténtelo con otro método de pago.")
 
 
 
