1427725397a35df98091ee08a686f16b98daed2b
maTeoBenites <mateorob.teo@gmail.com>
2026-08-12 20:22:43 -0500

    Refactor payment validation and processing logic
    Added time delay during payment processing and updated payment method validation to lowercase.

--- pagos.py
+++ pagos.py
@@ -1,22 +1,22 @@
 #⏳ Validar método de pago
 #⏳ Procesar pago
 #⏳ Confirmar pago
+import time
 
 
 # Se evalúa el método de pago
 def validar_metodo_pago(metodo_pago):
-    metodos_validos = ["Tarjeta",
-                        "Yape",
-                          "Plin",
-                            "PayPal",
-                              "Visa",
-                                "Mastercard"
+    metodos_validos = ["tarjeta",
+                        "yape",
+                          "plin",
+                            "payPal",
+                              "visa",
+                                "mastercard"
                                     ]
     if metodo_pago in metodos_validos:
         return True
     return False
-validar_metodo_depago = validar_metodo_pago("Visa")
-print(validar_metodo_depago)
+
 
 
 # Se procesa el pago
@@ -24,17 +24,18 @@ def procesar_pago(metodo_pago, monto):
 
     if validar_metodo_pago(metodo_pago):
       print(f"Procesando pago de S/{monto} con {metodo_pago}...")
+      time.sleep(1)
       return True
     else:
-      print("el sistema no pudo aceptar su método de pago")
+      print("el sistema no pudo aceptar su método de pago.")
       return False
-procesar_unpago = procesar_pago("Visa", 11111)
+
 
 
 # Finalmente el pago se confirma
 def confirmar_pago(pago_exitoso):
    if pago_exitoso:
-      print("Se a realizado exitosamente el pago")
+      print("Se a realizado exitosamente el pago.")
       return True
    else:
-      print("Ha ocurrido un error al confirmar el pago, inténtelo de nuevo")
+      print("Ha ocurrido un error al confirmar el pago, inténtelo de nuevo.")
